c4ef508ee6cf80fcc60d5760b61ecfb0a3abad62
zlamb1 <zlamb@my.wctc.edu>
2022-11-02 21:10:19 -0500

    Set up project to communicate with database

--- MovieLibrary/Program.cs
+++ MovieLibrary/Program.cs
@@ -1,5 +1,5 @@
-﻿using MovieLibrary.interfaces;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieLibraryEntities.Context;
 using System;
 
 namespace MovieLibrary
@@ -8,6 +8,15 @@ namespace MovieLibrary
     {
         static void Main(string[] args)
         {
+            // print all movies
+            using (var db = new MovieContext())
+            {
+                foreach (var movie in db.Movies)
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+
             var provider = new Startup().ConfigureServices();
             var aggregateMenu = provider.GetService<IMenu>();
             aggregateMenu.Start();
