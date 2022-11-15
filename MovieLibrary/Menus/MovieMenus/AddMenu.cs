b21c7c41df856d81f423a855e92719688a5a4cb8
zlamb1 <zlamb@my.wctc.edu>
2022-11-15 16:27:21 -0600

    updated add menu

--- MovieLibrary/Menus/MovieMenus/AddMenu.cs
+++ MovieLibrary/Menus/MovieMenus/AddMenu.cs
@@ -4,11 +4,6 @@ using MovieLibrary.utility;
 using MovieLibraryEntities.Context;
 using MovieLibraryEntities.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MovieLibrary.Menus.MovieMenus
 {
@@ -23,34 +18,34 @@ namespace MovieLibrary.Menus.MovieMenus
         {
             base.Start();
 
+            var movie = new Movie();
+
+            var title = InputUtility.GetStringWithPrompt("What is the title of the movie?\n");
+            if (string.IsNullOrEmpty(title))
+            {
+                Restart("The movie title cannot be null/blank!");
+                return;
+            }
+            movie.Title = title;
+
+            Console.WriteLine();
+
+            // TODO: add genre selection
+
+            var releaseDateStr = InputUtility.GetStringWithPrompt("What is the movie's release date (blank for current date)?\n");
+            if (string.IsNullOrEmpty(releaseDateStr))
+                movie.ReleaseDate = DateTime.Now;
+            else
+                movie.ReleaseDate = DateTime.Parse(releaseDateStr);
+
+            Console.WriteLine();
+            Console.WriteLine("Generated new movie => ");
+            Console.WriteLine("    Id: " + movie.Id);
+            Console.WriteLine("    Title: " + movie.Title);
+            Console.WriteLine("    Release Date: " + movie.ReleaseDate);
+
             using (var db = new MovieContext())
             {
-                Console.WriteLine();
-
-                Movie movie = new Movie();
-
-                var title = InputUtility.GetStringWithPrompt("What is the title of the movie? ");
-                if (string.IsNullOrEmpty(title))
-                {
-                    Restart("The movie title cannot be null/blank!");
-                    return;
-                }
-                movie.Title = title;
-
-                Console.WriteLine();
-
-                var releaseDateStr = InputUtility.GetStringWithPrompt("What is the movie's release date (blank for current date)? ");
-                if (string.IsNullOrEmpty(releaseDateStr))
-                    movie.ReleaseDate = DateTime.Now;
-                else
-                    movie.ReleaseDate = DateTime.Parse(releaseDateStr);
-
-                Console.WriteLine();
-                Console.WriteLine("Generated new movie => ");
-                Console.WriteLine("    Id: " + movie.Id);
-                Console.WriteLine("    Title: " + movie.Title);
-                Console.WriteLine("    Release Date: " + movie.ReleaseDate);
-
                 db.Add(movie);
                 db.SaveChanges();
             }
